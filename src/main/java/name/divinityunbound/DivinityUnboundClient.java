436e51969a907bb4b846e2c02b3b51fe64c55b84
Tyler Wilkins <lohrun@gmail.com>
2024-02-21 10:38:27 -0500

    Initial commit

---
+++ src/main/java/name/divinityunbound/DivinityUnboundClient.java
@@ -0,0 +1,10 @@
+package name.divinityunbound;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DivinityUnboundClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/name/divinityunbound/DivinityUnboundDataGenerator.java
@@ -0,0 +1,11 @@
+package name.divinityunbound;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DivinityUnboundDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
